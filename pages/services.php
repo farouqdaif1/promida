1b15c1a865f558d167b77bf9834a2f40df38fdfa
farouqdaif1 <daiffarouq@gmail.com>
2022-09-28 22:57:36 +0200

    Add content to services

--- pages/services.php
+++ pages/services.php
@@ -95,10 +95,8 @@
                             <div class="container">
                                 <div class="article-head">
                                     <h2>website & Mobile Apps creation & development</h2>
-                                    <p>A brands.com experience is its best opportunity to control its own message. Therefore. 
-                                        we create content with a strong communications purpose while taking into consideration consumer insights. We then build elegant, durable solutions to key business challenges across all touchpoints, screens, and Devices.We Craft beautiful, elegant, user-friendly,secure and stable websites for your business that represents your Brand Image.
-                                        And Showcase your services or products in the Best way.</p>                            </div>
-
+                                    <p>A brands.com experience is its best opportunity to control its own message. Therefore. we create content with a strong communications purpose while taking into consideration consumer insights. We then build elegant, durable solutions to key business challenges across all touchpoints, screens, and Devices.</p>
+                                </div>
                                 <div class="circle-container">
                                     <div class="circle">
                                         <img id="web-image" class="header-image"src="../images/website.png" alt="computer-image">
@@ -138,9 +136,8 @@
                             <div class="container">
                                 <div class="article-head">
                                     <h2>Social Media Marketing</h2>
-                                    <p>A brands.com experience is its best opportunity to control its own message. Therefore. 
-                                        we create content with a strong communications purpose while taking into consideration consumer insights. We then build elegant, durable solutions to key business challenges across all touchpoints, screens, and Devices.We Craft beautiful, elegant, user-friendly,secure and stable websites for your business that represents your Brand Image.
-                                        And Showcase your services or products in the Best way.</p>                            </div>
+                                    <p>we generate ideas, branded content, and selectively distribute it across owned and paid Social Media Channels; Facebook, Twitter,Youtube, Pinterest, Instagram. etc., depending on a holistic data-driven strategy.</p>                            
+                                </div>
 
                                 <div class="circle-container">
                                     <div class="circle">
@@ -151,21 +148,33 @@
                     </div>
                     <div class="work-process">
                         <div class="container">
-                            <h2>Work Process</h2>
-                            <p>A brands.com experience is its best opportunity to control its own message. Therefore. 
-                            we create content with a strong communications purpose while taking into consideration consumer insights. We then build elegant, durable solutions to key business challenges across all touchpoints, screens, and Devices.We Craft beautiful, elegant, user-friendly,secure and stable websites for your business that represents your Brand Image.
-                            And Showcase your services or products in the Best way.</p>
-
-                            <p>A brands.com experience is its best opportunity to control its own message. Therefore. 
-                            we create content with a strong communications purpose while taking into consideration consumer insights. We then build elegant, durable solutions to key business challenges across all touchpoints, screens, and Devices.We Craft beautiful, elegant, user-friendly,secure and stable websites for your business that represents your Brand Image.
-                            And Showcase your services or products in the Best way.</p>
-
-                            <p>A brands.com experience is its best opportunity to control its own message. Therefore. 
-                            we create content with a strong communications purpose while taking into consideration consumer insights. We then build elegant, durable solutions to key business challenges across all touchpoints, screens, and Devices.We Craft beautiful, elegant, user-friendly,secure and stable websites for your business that represents your Brand Image.
-                            And Showcase your services or products in the Best way.</p>
+                            <h2>Social Media </h2>
+                            <ul>
+                                <li>Advertising Targeted Campaigns </li>
+                                <li>Increasing Brand Awareness </li>
+                                <li>Building Community </li>
+                                <li>Customer Education </li>
+                                <li> Customer-Caring  </li>
+                                <li> Originating Leads </li>
+                            </ul>
+                            <h6>Advertising Targeted Campaigns</h6>
+                            <p>We are working inside the market itself We see the potential Customers in Each season and their needs & we target them by User-Generated Content and Creative theme to keep them up-to-date to your Business .</p>
+                            <h6>Increasing Brand Awareness</h6>
+                            <p>Our main object is to penetrate the Market with your Business and increase the Word of Mouth on Social Media “ PTAT “ </p>
+                            <h6>Building Community </h6>
+                            <p>After Building Community they always eager to know more about your Business And here Comes our role in Educating  and Guiding them</p>
+                            <ul id="community">
+                                <li>We Generated Advertising Campaigns and Created your Identity </li>
+                                <li>Then we increase “PTAT “ So the result we have a group of people aware of your Business And this is called  Community 
+                                By this we can say we have a good Marketing Booster </li>
+                            </ul>
+                            <h6>Customer Education </h6>
+                            <p>After Building Community they always eager to know more about your Business And here Comes our role in Educating  and Guiding them</p>
+                            <h6>Customer- Caring  </h6>
+                            <p>There is no successful marketing without bad feedbacks or suggestions We are leaders in listening and satisfying your Customers or even Ours  </p>
+                            <h6>Originating Leads </h6>
+                            <p>We have Created and Educated this Community So, all they need some information about the product or service itself and some Contact information to do the “ Call to Action “ </p>
                         </div>
-                        <img class="layer"src="../images/layer-12@2x.png" alt="layer">
-
                     </div>
                     <div class="some-of-our-project">
                         <div class="container">
@@ -277,9 +286,7 @@
                             <div class="container">
                                 <div class="article-head">
                                     <h2>Creating Strategies</h2>
-                                    <p>A brands.com experience is its best opportunity to control its own message. Therefore. 
-                                        we create content with a strong communications purpose while taking into consideration consumer insights. We then build elegant, durable solutions to key business challenges across all touchpoints, screens, and Devices.We Craft beautiful, elegant, user-friendly,secure and stable websites for your business that represents your Brand Image.
-                                        And Showcase your services or products in the Best way.</p>                            </div>
+                                    <p>It is an Important step to know what is the perfect plan for the business itself By generating KPI’s and researching the market </p>                            </div>
                                 <div class="circle-container">
                                     <div class="circle">
                                         <img  id="strategies-image"  class="header-image"src="../images/startegy.png" alt="computer-image">
