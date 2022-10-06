2ca74605850db89cced0a984ac3efff02420279f
farouqdaif1 <daiffarouq@gmail.com>
2022-10-06 20:31:25 +0200

    Fix our story

--- pages/subcode/workwith.php
+++ pages/subcode/workwith.php
@@ -36,9 +36,6 @@
                                 }
                                 ?>
                 </div>
-                <!-- If we need navigation buttons -->
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-button-next"></div>  
             </div>
         </div>
     </div>
